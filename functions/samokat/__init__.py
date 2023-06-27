3cafe0a6ad2a8ce180b5563859eaf694d27f5c47
k.kosmachev <k.kosmachev@mail.ru>
2023-06-27 14:51:23 +0300

    Import modules and package

---
+++ functions/samokat/__init__.py
@@ -0,0 +1,12 @@
+
+from functions.samokat.address import (
+    input_first_address,
+    input_new_address,
+)
+from functions.samokat.auth import (
+    logout,
+    login,
+)
+from functions.samokat.cart import (
+    add_to_cart,
+)

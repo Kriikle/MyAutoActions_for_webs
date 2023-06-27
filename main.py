155e45250151a320c7e05507bfbb4a641867cb6c
k.kosmachev <k.kosmachev@mail.ru>
2023-06-27 14:52:44 +0300

    Main script changes

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 # This is a sample Python script.
 import time
+import functions.samokat as sm
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from functions.samokat.address import input_first_address
 
-from functions.samokat.auth import login,logout
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     browser = webdriver.Firefox()
     browser.get('https://web.samokat.ru/')
     time.sleep(3)
+    sm.input_first_address(browser, 'Санкт-Петербург', 'Каменноостровский проспект', '17', 93)
+    time.sleep(3)
+    sm.add_to_cart(browser, 1)
+
     # login(browser, '9212341221')
     # time.sleep(3)
     # logout(browser)
-    # input_first_address(browser, 'Санкт-Петербург', 'Каменноостровский проспект', '12', 93)
-    # elem_find = browser.find_element(By.CLASS_NAME, '_input_1pil0_25')
-    # elem_find.send_keys('Закуски к пиву')
+    #
+    #
 
-    browser.quit()
+    # browser.quit()
